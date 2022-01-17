9cc99c3f5072319013ec13455247fb8a02634612
Viennarz Curtiz <viennarzc@gmail.com>
2022-01-17 13:58:56 +0800

    Create project

---
+++ SwiftUI-IVO/SwiftUI-IVO/SwiftUI_IVOApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_IVOApp.swift
+//  SwiftUI-IVO
+//
+//  Created by Viennarz Curtiz on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_IVOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
